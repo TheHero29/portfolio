555e6af49665be02ac368475d733ed508e1fc38a
TheHero29 <loukik.23bcs10059@ms.sst.scaler.com>
2024-06-07 13:08:45 +0530

    more skills

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -5,6 +5,8 @@ import './Skills.css'
 import React from 'react';
 import { FaJava, FaPython, FaHtml5, FaCss3, FaJs, FaReact } from 'react-icons/fa';
 import { DiMysql} from 'react-icons/di';
+import { FaLinux, FaNodeJs } from 'react-icons/fa';
+import { SiMongodb,SiExpress,SiGnubash,SiTypescript, SiSpring } from 'react-icons/si';
 
 function Skills(){
     return (
@@ -12,7 +14,6 @@ function Skills(){
             <Header />
             <h1>Skills</h1>
             <div className="skills-main-container">
-            
                     <div class="scard"><div class="iconContainer"><FaJava className="sicon" size={50} /></div></div>
                     <div class="scard"><div class="iconContainer"><DiMysql className="sicon" size={50} /></div></div>
                     <div class="scard"><div class="iconContainer"><FaPython className="sicon" size={50} /></div></div>
@@ -20,6 +21,14 @@ function Skills(){
                     <div class="scard"><div class="iconContainer"><FaCss3 className="sicon" size={50} /></div></div>
                     <div class="scard"><div class="iconContainer"><FaJs className="sicon" size={50} /></div></div>
                     <div class="scard"><div class="iconContainer"><FaReact className="sicon" size={50} /></div></div>
+                    <div class="scard"><div class="iconContainer"><SiGnubash className="sicon" size={50} /></div></div>
+                    <div class="scard"><div class="iconContainer"><FaNodeJs className="sicon" size={50} /></div></div>
+                    <div class="scard"><div class="iconContainer"><SiTypescript className="sicon" size={50} /></div></div>
+                    <div class="scard"><div class="iconContainer"><SiSpring className="sicon" size={50} /></div></div>
+                    <div class="scard"><div class="iconContainer"><SiExpress className="sicon" size={50} /></div></div>
+                    <div class="scard"><div class="iconContainer"><SiMongodb className="sicon" size={50} /></div></div>
+                    <div class="scard"><div class="iconContainer"><FaLinux className="sicon" size={50} /></div></div>
+
             </div>
             <Footer />
         </div>
