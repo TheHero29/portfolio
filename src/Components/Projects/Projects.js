72b219af9f133744d04db03427a2a7386f34ed9a
TheHero29 <loukik.23bcs10059@ms.sst.scaler.com>
2024-06-09 12:11:06 +0530

    fixed project cards

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -7,10 +7,10 @@ function Projects(){
         <div className='projects'>
             <Header />
             <div className='main-container'>
-                <ProjectCard heading={"Image Editor"} desc={"Made image editor using java"} src={"https://www.google.com"}/>
-                <ProjectCard heading={"Quiz App"} desc={"Made Quiz App using HTML,CSS and javascript"} src={"https://www.google.com"}/>
+                <ProjectCard heading={"Image Editor"} desc={"Made image editor using java"} src={"https://github.com/TheHero29/ImageEditor"}/>
+                <ProjectCard heading={"Quiz App"} desc={"Made Quiz App using HTML,CSS and javascript"} src={"https://github.com/TheHero29/Quiz-app"}/>
                 <ProjectCard heading={"Web Scrapper"} desc={"Made web scrapper using python"} src={"https://www.google.com"}/>
-                <ProjectCard heading={"Vehicle-management-system"} desc={"Made vehicle management system using springboot"} src={"https://www.google.com"}/>
+                <ProjectCard heading={"Management-system"} desc={"Contributed to vehicle service management system using springboot"} src={"https://github.com/yasharya2901/VehicleServiceMangement"}/>
             </div>
             <Footer />
         </div>
